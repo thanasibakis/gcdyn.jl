f07fdcbbdf8119a594758f3f5308df5749872cb3
Thanasi Bakis <abakis@uci.edu>
2023-08-17 16:09:00 -0400

    Appx and full models

--- inference-demo/posterior_medians.jl
+++ inference-demo/posterior_medians.jl
@@ -18,7 +18,7 @@ sigmoid(x, xscale, xshift, yscale, yshift) = yscale * expit(xscale * (x - xshift
 const λ_truth = x -> sigmoid(x, true_params[:xscale], true_params[:xshift], true_params[:yscale], true_params[:yshift])
 const truth = MultitypeBranchingProcess(λ_truth, true_params[:mu], 2, [2, 4, 6, 8], 1, 0, 2)
 
-@model function Model(trees::Vector{TreeNode})
+@model function FullModel(trees::Vector{TreeNode})
     xscale ~ Gamma(2, 1)
     xshift ~ Normal(5, 1)
     yscale ~ Gamma(2, 1)
@@ -36,6 +36,24 @@ const truth = MultitypeBranchingProcess(λ_truth, true_params[:mu], 2, [2, 4, 6,
     Turing.@addlogprob! loglikelihood(sampled_model, trees)
 end
 
+@model function AppxModel(trees::Vector{TreeNode})
+    xscale ~ Gamma(2, 1)
+    xshift ~ Normal(5, 1)
+    yscale ~ Gamma(2, 1)
+    yshift ~ Gamma(1, 1)
+
+    μ ~ LogNormal(0, 0.3)
+    # γ ~ LogNormal(1.5, 1)
+
+    λ = x -> sigmoid(x, xscale, xshift, yscale, yshift)
+
+    sampled_model = MultitypeBranchingProcess(
+        λ, μ, truth.γ, truth.state_space, truth.transition_matrix, truth.ρ, truth.σ, truth.present_time
+    )
+
+    Turing.@addlogprob! sum(stadler_appx_loglikelhood(sampled_model, tree) for tree in trees)
+end
+
 println("Sampling...")
 
 NUM_TREESETS = 100
@@ -46,7 +64,7 @@ Threads.@threads for i in 1:NUM_TREESETS
     trees = rand_tree(truth, 30, truth.state_space[1]);
 
     chns[i] = sample(
-        Model(trees),
+        AppxModel(trees),
         MH(
             :xscale => x -> LogNormal(log(x), 1),
             :xshift => x -> Normal(x, 1),
