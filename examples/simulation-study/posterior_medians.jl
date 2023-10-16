19da6551e4c351643270bd3f3d63d4da181fa270
Thanasi Bakis <abakis@uci.edu>
2023-10-16 15:58:14 -0700

    Updated sim study for larger trees

--- examples/simulation-study/posterior_medians.jl
+++ examples/simulation-study/posterior_medians.jl
@@ -2,20 +2,13 @@ println("Loading packages...")
 
 using gcdyn, CSV, DataFrames, Turing, StatsPlots
 
-println("Setting up model...")
-
-# https://docs.julialang.org/en/v1/manual/performance-tips/index.html#Avoid-untyped-global-variables
-const transition_p = 0.3
-const transition_δ = 0.8
-const truth = SigmoidalBirthRateBranchingProcess(1, 5, 1.5, 1, 1.3, 1.3, [2, 4, 6, 8], random_walk_transition_matrix([2, 4, 6, 8], transition_p; δ=transition_δ), 1, 0, 2)
-
-@model function Model(trees::Vector{TreeNode})
+@model function Model(trees::Vector{TreeNode}, truth::AbstractBranchingProcess)
     xscale  ~ Gamma(2, 1)
     xshift  ~ Normal(5, 1)
     yscale  ~ Gamma(2, 1)
     yshift  ~ Gamma(1, 1)
-    μ       ~ LogNormal(0, 0.3)
-    γ       ~ LogNormal(0, .5)
+    μ       ~ LogNormal(0, 0.5)
+    γ       ~ LogNormal(0, 0.5)
     logit_p ~ Normal(0, 1.8)
     logit_δ ~ Normal(1.4, 1.4)
 
@@ -29,86 +22,92 @@ const truth = SigmoidalBirthRateBranchingProcess(1, 5, 1.5, 1, 1.3, 1.3, [2, 4, 
     Turing.@addlogprob! loglikelihood(sampled_model, trees; reltol=1e-3, abstol=1e-3)
 end
 
-println("Sampling...")
+function main()
+    println("Setting up model...")
 
-# https://docs.julialang.org/en/v1/manual/style-guide/#Write-functions,-not-just-scripts
-function run_simulations(num_treesets, num_trees, num_samples)
-    chns = Vector{Chains}(undef, num_treesets)
+    transition_p = 0.3
+    transition_δ = 0.8
+    truth = SigmoidalBirthRateBranchingProcess(1, 5, 1.5, 1, 1.3, 1.3, [2, 4, 6, 8], random_walk_transition_matrix([2, 4, 6, 8], transition_p; δ=transition_δ), 1, 0, 3)
+
+    println("Sampling from prior...")
+
+    prior_samples = sample(Model(rand_tree(truth, 1, truth.state_space[1]), truth), Prior(), 100) |> DataFrame
+    prior_samples.p = gcdyn.expit.(prior_samples[:, :logit_p])
+    prior_samples.δ = gcdyn.expit.(prior_samples[:, :logit_δ])
+
+    println("Sampling from posterior...")
+
+    num_treesets = 100
+    dfs = Vector{DataFrame}(undef, num_treesets)
 
     Threads.@threads for i in 1:num_treesets
-        trees = rand_tree(truth, num_trees, truth.state_space[1]);
+        trees = rand_tree(truth, 15, truth.state_space[1]);
 
-        chns[i] = sample(
-            Model(trees),
+        dfs[i] = sample(
+            Model(trees, truth),
             MH(
-                :xscale => x -> LogNormal(log(x), 0.3),
-                :xshift => x -> Normal(x, 1),
-                :yscale => x -> LogNormal(log(x), 0.3),
-                :yshift => x -> LogNormal(log(x), 0.3),
-                :μ => x -> LogNormal(log(x), 0.3),
-                :γ => x -> LogNormal(log(x), 0.3),
-                :logit_p => x -> Normal(x, 0.5),
-                :logit_δ => x -> Normal(x, 0.5)
+                :xscale => x -> LogNormal(log(x), 0.2),
+                :xshift => x -> Normal(x, 0.7),
+                :yscale => x -> LogNormal(log(x), 0.2),
+                :yshift => x -> LogNormal(log(x), 0.2),
+                :μ => x -> LogNormal(log(x), 0.2),
+                :γ => x -> LogNormal(log(x), 0.2),
+                :logit_p => x -> Normal(x, 0.2),
+                :logit_δ => x -> Normal(x, 0.2)
             ),
-            num_samples
-        )
+            5000
+        ) |> DataFrame
+
+        dfs[i].run .= i
     end
 
-    chns
+    println("Exporting samples...")
+    
+    posterior_samples = vcat(dfs...)
+    CSV.write("posterior-samples.csv", posterior_samples)
+
+    println("Visualizing...")
+
+    medians = combine(
+        groupby(posterior_samples, :run),
+        :xscale => median => :xscale,
+        :xshift => median => :xshift,
+        :yscale => median => :yscale,
+        :yshift => median => :yshift,
+        :μ => median => :μ,
+        :γ => median => :γ,
+        :logit_p => (x -> gcdyn.expit(median(x))) => :p,
+        :logit_δ => (x -> gcdyn.expit(median(x))) => :δ
+    )
+    select!(medians, Not(:run))
+
+    hists = map(propertynames(medians)) do param
+        histogram(prior_samples[:, param]; normalize=:pdf, label="Prior", fill="grey")
+        histogram!(medians[!, param]; normalize=:pdf, fill="lightblue", alpha=0.7, label="Medians")
+    
+        true_value =
+            if param == :p
+                transition_p
+            elseif param == :δ
+                transition_δ
+            else
+                getfield(truth, param)
+            end
+    
+        vline!([true_value]; label="Truth", color="#1A4F87", width=6)
+        title!(string(param))
+    end
+    
+    plot(hists...;
+        layout=(2, 4),
+        thickness_scaling=2.25,
+        size=(3600, 1200),
+        plot_title="Posterior median sampling distribution"
+    )
+
+    png("posterior-medians.png")
+    
+    println("Done!")
 end
 
-chns = run_simulations(100, 15, 5000)
-
-println("Exporting samples...")
-
-dfs = map(enumerate(chns)) do (i, chn)
-    df = DataFrame(chn)
-    df.run .= i
-    df
-end
-
-df = vcat(dfs...)
-CSV.write("samples.csv", df)
-
-medians = combine(
-    groupby(df, :run),
-    :xscale => median => :xscale,
-    :xshift => median => :xshift,
-    :yscale => median => :yscale,
-    :yshift => median => :yshift,
-    :μ => median => :μ,
-    :γ => median => :γ,
-    :logit_p => (x -> gcdyn.expit(median(x))) => :p,
-    :logit_δ => (x -> gcdyn.expit(median(x))) => :δ
-)
-select!(medians, Not(:run))
-
-println("Visualizing...")
-
-hists = map(propertynames(medians)) do param
-    histogram(medians[!, param]; normalize=:pdf, label="Medians")
-
-    true_value =
-        if param == :p
-            transition_p
-        elseif param == :δ
-            transition_δ
-        else
-            getfield(truth, param)
-        end
-
-    vline!([true_value]; label="Truth", linewidth=4)
-    #plot!(priors[param]; label="Prior", fill=(0, 0.5))
-    title!(string(param))
-end
-
-plot(hists...;
-    layout=(4, 2),
-    thickness_scaling=0.75,
-    dpi=300,
-    size=(800, 600),
-    plot_title="Posterior median sampling distribution"
-)
-png("posterior_medians.png")
-
-println("Done!")
+main()
