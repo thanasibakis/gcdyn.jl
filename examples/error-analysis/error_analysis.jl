fdbf35ba10a05d7923e2e67b2adda47721b109f1
Thanasi Bakis <abakis@uci.edu>
2023-11-05 14:27:05 -0800

    Updated error-analysis

--- examples/error-analysis/error_analysis.jl
+++ examples/error-analysis/error_analysis.jl
@@ -44,7 +44,7 @@ function run_simulations(truth)
     for (num_trees, chns_dict) in chns
         Threads.@threads for i in 1:NUM_TREESETS
             trees = rand_tree(truth, num_trees, truth.state_space[1])
-    
+
             chns_dict[:corrected][i] = sample(
                 CorrectedModel(trees, truth),
                 MH(
@@ -53,7 +53,7 @@ function run_simulations(truth)
                 ),
                 2000
             )
-    
+
             chns_dict[:original][i] = sample(
                 OriginalModel(trees, truth),
                 MH(
@@ -71,7 +71,7 @@ end
 function visualize_original(chns, truth)
     λ_plts = []
     μ_plts = []
-    
+
     for n in sort(collect(keys(chns)))
         chns_dict = chns[n]
 
@@ -79,42 +79,42 @@ function visualize_original(chns, truth)
         medians_original = map(chns_dict[:original]) do chn
             median(get(chn, :λ)[1])
         end
-    
+
         plt = histogram(medians_original; alpha=0.7, label="Original", normalize=:pdf, fill="grey")
         vline!([truth.λ]; label="Truth", linewidth=4, color="orange")
         xlims!(0, 3)
         title!("$n trees", titlefontsize=10)
-    
+
         push!(λ_plts, plt)
-    
+
         # Death rate plots
         medians_original = map(chns_dict[:original]) do chn
             median(get(chn, :μ)[1])
         end
-    
+
         plt = histogram(medians_original; alpha=0.7, label="Original", normalize=:pdf, fill="grey")
         vline!([truth.μ]; label="Truth", linewidth=4, color="orange")
         xlims!(0, 3)
         title!("$n trees", titlefontsize=10)
-    
+
         push!(μ_plts, plt)
     end
-    
-    plot(λ_plts...; layout=(1, 4), thickness_scaling=0.75, dpi=300, size=(1000, 250), plot_title="Birth rate sampling distributions", plot_titlefontsize=12)
-    xlabel!("Posterior median")
-    ylabel!("Density")
+
+    plot(λ_plts...; layout=(1, 4), thickness_scaling=0.75, dpi=300, size=(1000, 300), plot_title="Birth rate sampling distributions", plot_titlefontsize=12)
+    #xlabel!("Posterior median")
+    #ylabel!("Density")
     png("birth-rate-errors-original-only.png")
-    
-    plot(μ_plts...; layout=(1, 4), thickness_scaling=0.75, dpi=300, size=(1000, 250), plot_title="Death rate sampling distributions", plot_titlefontsize=12)
-    xlabel!("Posterior median")
-    ylabel!("Density")
+
+    plot(μ_plts...; layout=(1, 4), thickness_scaling=0.75, dpi=300, size=(1000, 300), plot_title="Death rate sampling distributions", plot_titlefontsize=12)
+    #xlabel!("Posterior median")
+    #ylabel!("Density")
     png("death-rate-errors-original-only.png")
 end
 
 function visualize_both(chns, truth)
     λ_plts = []
     μ_plts = []
-    
+
     for n in sort(collect(keys(chns)))
         chns_dict = chns[n]
 
@@ -125,15 +125,15 @@ function visualize_both(chns, truth)
         medians_original = map(chns_dict[:original]) do chn
             median(get(chn, :λ)[1])
         end
-    
+
         plt = histogram(medians_original; alpha=0.7, label="Original", normalize=:pdf, fill="grey")
         histogram!(medians_corrected; alpha=0.7, label="Corrected", normalize=:pdf, fill="lightblue")
         vline!([truth.λ]; label="Truth", linewidth=4, color="orange")
         xlims!(0, 3)
         title!("$n trees", titlefontsize=10)
-    
+
         push!(λ_plts, plt)
-    
+
         # Death rate plots
         medians_corrected = map(chns_dict[:corrected]) do chn
             median(get(chn, :μ)[1])
@@ -141,24 +141,24 @@ function visualize_both(chns, truth)
         medians_original = map(chns_dict[:original]) do chn
             median(get(chn, :μ)[1])
         end
-    
+
         plt = histogram(medians_original; alpha=0.7, label="Original", normalize=:pdf, fill="grey")
         histogram!(medians_corrected; alpha=0.7, label="Corrected", normalize=:pdf, fill="lightblue")
         vline!([truth.μ]; label="Truth", linewidth=4, color="orange")
         xlims!(0, 3)
         title!("$n trees", titlefontsize=10)
-    
+
         push!(μ_plts, plt)
     end
-    
-    plot(λ_plts...; layout=(1, 4), thickness_scaling=0.75, dpi=300, size=(1000, 250), plot_title="Birth rate sampling distributions", plot_titlefontsize=12)
-    xlabel!("Posterior median")
-    ylabel!("Density")
+
+    plot(λ_plts...; layout=(1, 4), thickness_scaling=0.75, dpi=300, size=(1000, 300), plot_title="Birth rate sampling distributions", plot_titlefontsize=12)
+    #xlabel!("Posterior median")
+    #ylabel!("Density")
     png("birth-rate-errors.png")
-    
-    plot(μ_plts...; layout=(1, 4), thickness_scaling=0.75, dpi=300, size=(1000, 250), plot_title="Death rate sampling distributions", plot_titlefontsize=12)
-    xlabel!("Posterior median")
-    ylabel!("Density")
+
+    plot(μ_plts...; layout=(1, 4), thickness_scaling=0.75, dpi=300, size=(1000, 300), plot_title="Death rate sampling distributions", plot_titlefontsize=12)
+    #xlabel!("Posterior median")
+    #ylabel!("Density")
     png("death-rate-errors.png")
 end
 
