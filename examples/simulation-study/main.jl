394ad9ddc91a9a7de7cb021727c44cbbdf9de8ba
Thanasi Bakis <abakis@uci.edu>
2024-02-16 12:07:42 -0800

    Simulation study using new parameterization

--- examples/simulation-study/main.jl
+++ examples/simulation-study/main.jl
@@ -1,20 +1,29 @@
 println("Loading packages...")
 
-using gcdyn, CSV, DataFrames, Turing, StatsPlots
+using gcdyn, CSV, DataFrames, Random, Turing
 
 @model function Model(trees, Γ, state_space, present_time)
-    λ_xscale  ~ Gamma(2, 1)
-    λ_xshift  ~ Normal(5, 1)
-    λ_yscale  ~ Gamma(2, 1)
-    λ_yshift  ~ Gamma(1, 1)
-    μ       ~ LogNormal(0, 0.5)
-    δ       ~ LogNormal(0, 0.5)
+    # Keep priors on the same scale for NUTS
+    log_λ_xscale ~ Normal(0, 1)
+    λ_xshift⁻    ~ Normal(0, 1)
+    log_λ_yscale ~ Normal(0, 1)
+    log_λ_yshift ~ Normal(0, 1)
+    log_μ        ~ Normal(0, 1)
+    log_δ        ~ Normal(0, 1)
+
+    # Obtain our actual parameters from the proxies
+    λ_xscale = exp(log_λ_xscale * 0.75 + 0.5)
+    λ_xshift = λ_xshift⁻ + 5
+    λ_yscale = exp(log_λ_yscale * 0.75 + 0.5)
+    λ_yshift = exp(log_λ_yshift * 1.2 - 0.5)
+    μ        = exp(log_μ * 0.5)
+    δ        = exp(log_δ * 0.5)
 
     sampled_model = VaryingTypeChangeRateBranchingProcess(
         λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ, Γ, 1, 0, state_space, present_time
     )
 
-    # TODO: remove the ismissing check once you learn how Turing prior sampling works
+    # TODO: remove the ismissing check once Turing is patched
     if DynamicPPL.leafcontext(__context__) !== Turing.PriorContext() && !ismissing(trees)
         Turing.@addlogprob! loglikelihood(sampled_model, trees)
     end
@@ -23,143 +32,38 @@ end
 function main()
 	println("Setting up model...")
 
-    Γ = [-4 2 1 1; 1 -4 2 1; 2 1 -4 1; 1 2 1 -4]
-    truth = VaryingTypeChangeRateBranchingProcess(1, 5, 1.5, 1, 1.3, 0.25, Γ, 1, 0, [2, 4, 6, 8], 3)
+    Γ = [-1 0.5 0.25 0.25; 2 -4 1 1; 2 2 -5 1; 0.125 0.125 0.25 -0.5]
+    truth = VaryingTypeChangeRateBranchingProcess(1, 5, 1.5, 1, 1.3, 1, Γ, 1, 0, [2, 4, 6, 8], 3)
 
-	println("Sampling from prior...")
-    prior_samples = sample(Model(missing, truth.Γ, truth.state_space, truth.present_time), Prior(), 100) |> DataFrame
+	# println("Sampling from prior...")
+    # prior_samples = sample(Model(missing, truth.Γ, truth.state_space, truth.present_time), Prior(), 100) |> DataFrame
 
 	println("Sampling from posterior...")
     num_treesets = 100
 	num_trees_per_set = 15
     dfs = Vector{DataFrame}(undef, num_treesets)
 
+    # Let's export each run to a separate CSV file in case our program is OOM killed
+    tmp_dir = "tmp-" * randstring(10)
+    mkpath(tmp_dir)
+
     Threads.@threads for i in 1:num_treesets
         treeset = rand_tree(truth, num_trees_per_set, truth.state_space[1]);
 
         dfs[i] = sample(
             Model(treeset, truth.Γ, truth.state_space, truth.present_time),
-            MH(
-                :λ_xscale => x -> LogNormal(log(x), 0.2),
-                :λ_xshift => x -> Normal(x, 0.7),
-                :λ_yscale => x -> LogNormal(log(x), 0.2),
-                :λ_yshift => x -> LogNormal(log(x), 0.2),
-                :μ => x -> LogNormal(log(x), 0.2),
-                :δ => x -> LogNormal(log(x), 0.2),
-            ),
-            # Gibbs(
-            #     MH(:λ_xscale => x -> LogNormal(log(x), 0.2)),
-            #     MH(:λ_xshift => x -> Normal(x, 0.7)),
-            #     MH(:λ_yscale => x -> LogNormal(log(x), 0.2)),
-            #     MH(:λ_yshift => x -> LogNormal(log(x), 0.2)),
-            #     MH(:μ => x -> LogNormal(log(x), 0.2)),
-            #     MH(:δ => x -> LogNormal(log(x), 0.2)),
-            # ),
+            NUTS(),
             5000
         ) |> DataFrame
 
         dfs[i].run .= i
+        CSV.write("$tmp_dir/posterior-samples-$i.csv", dfs[i])
     end
 
     println("Exporting samples...")
     posterior_samples = vcat(dfs...)
     CSV.write("posterior-samples.csv", posterior_samples)
-
-	println("Visualizing...")
-	medians = combine(
-        groupby(posterior_samples, :run),
-        :λ_xscale => median => :λ_xscale,
-        :λ_xshift => median => :λ_xshift,
-        :λ_yscale => median => :λ_yscale,
-        :λ_yshift => median => :λ_yshift,
-        :μ => median => :μ,
-        :δ => median => :δ
-    )
-    select!(medians, Not(:run))
-
-    quantiles_025 = combine(
-        groupby(posterior_samples, :run),
-        :λ_xscale => (x -> quantile(x, 0.025)) => :λ_xscale,
-        :λ_xshift => (x -> quantile(x, 0.025)) => :λ_xshift,
-        :λ_yscale => (x -> quantile(x, 0.025)) => :λ_yscale,
-        :λ_yshift => (x -> quantile(x, 0.025)) => :λ_yshift,
-        :μ => (x -> quantile(x, 0.025)) => :μ,
-        :δ=> (x -> quantile(x, 0.025)) => :δ,
-    )
-    select!(quantiles_025, Not(:run))
-
-    quantiles_975 = combine(
-        groupby(posterior_samples, :run),
-        :λ_xscale => (x -> quantile(x, 0.975)) => :λ_xscale,
-        :λ_xshift => (x -> quantile(x, 0.975)) => :λ_xshift,
-        :λ_yscale => (x -> quantile(x, 0.975)) => :λ_yscale,
-        :λ_yshift => (x -> quantile(x, 0.975)) => :λ_yshift,
-        :μ => (x -> quantile(x, 0.975)) => :μ,
-        :δ => (x -> quantile(x, 0.975)) => :δ,
-    )
-    select!(quantiles_975, Not(:run))
-
-    median_hists = []
-    error_hists = []
-    ci_length_hists = []
-
-    for param in propertynames(medians)
-        true_value = getfield(truth, param)
-
-        median_hist = histogram(prior_samples[:, param]; normalize=:pdf, label="Prior", fill="grey")
-        histogram!(medians[!, param]; normalize=:pdf, fill="lightblue", alpha=0.7, label="Medians")
-        vline!([true_value]; label="Truth", color="#1A4F87", width=6)
-        title!(string(param))
-        push!(median_hists, median_hist)
-
-        relative_errors = (medians[:, param] .- true_value) ./ true_value
-        error_hist = histogram(relative_errors; normalize=:pdf, fill="lightblue", alpha=0.7, label="Errors")
-        xlims!((-3, 3))
-        title!(string(param))
-        push!(error_hists, error_hist)
-
-        ci_lengths = (quantiles_975[:, param] .- quantiles_025[:, param]) ./ true_value
-        ci_length_hist = histogram(ci_lengths; normalize=:pdf, fill="lightblue", alpha=0.7, label="CI length")
-        xlims!((0, xlims()[2]))
-        title!(string(param))
-        push!(ci_length_hists, ci_length_hist)
-    end
-    
-    plot(median_hists...;
-        layout=(2, 4),
-        thickness_scaling=2.25,
-        size=(3600, 1200),
-        plot_title="Posterior median sampling distribution"
-    )
-
-    png("posterior-medians.png")
-    
-    plot(error_hists...;
-        layout=(2, 4),
-        thickness_scaling=2.25,
-        size=(3600, 1200),
-        plot_title="Relative error distribution of posterior median"
-    )
-
-    png("relative-errors.png")
-    
-    plot(ci_length_hists...;
-        layout=(2, 4),
-        thickness_scaling=2.25,
-        size=(3600, 1200),
-        plot_title="Length distribution of 95% CIs (normalized)"
-    )
-
-    png("ci-lengths.png")
-
-    open("ci-coverage-proportions.txt", "w") do file
-        println(file, "95% Coverage proportions", "\n")
-        map(propertynames(quantiles_025)) do param
-            true_value = getfield(truth, param)
-            coverage = quantiles_025[:, param] .<= true_value .<= quantiles_975[:, param]
-            println(file, string(param), ": ", mean(coverage))
-        end
-    end
+    rm(tmp_dir, recursive=true)
 
 	println("Done!")
 end
---
+++ examples/simulation-study/merge_and_visualize.R
@@ -0,0 +1,89 @@
+library(tidyverse)
+
+# Get the list of all files in the directory and subdirectories
+all_files <- dir(path = ".", full.names = TRUE, recursive = TRUE)
+
+# Filter the list to include only CSV files in directories starting with "tmp-"
+csv_files <- grep("tmp-.*/.*\\.csv$", all_files, value = TRUE)
+
+# Read all the CSV files into a single data frame, updating the run column to be unique
+posterior_samples <- lapply(csv_files, read_csv) |>
+	bind_rows(.id = "run") |>
+	mutate(run = as.numeric(run))
+
+prior_samples <- tibble(
+	λ_xscale = rlnorm(1000, 0.5, 0.75),
+	λ_xshift = rnorm(1000, 5, 1),
+	λ_yscale = rlnorm(1000, 0.5, 0.75),
+	λ_yshift = rlnorm(1000, -0.5, 1.2),
+	μ        = rlnorm(1000, 0, 0.5),
+	δ        = rlnorm(1000, 0, 0.5)
+) |>
+	pivot_longer(c(λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ), names_to = "Parameter", values_to = "Sample")
+
+truth <- tibble(
+	λ_xscale = 1,
+	λ_xshift = 5,
+	λ_yscale = 1.5,
+	λ_yshift = 1,
+	μ        = 1.3,
+	δ        = 1
+) |>
+	pivot_longer(c(λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ), names_to = "Parameter", values_to = "Truth")
+
+posterior_summaries <- posterior_samples |>
+	rename(λ_xshift_ = "λ_xshift⁻") |>
+	mutate(
+		λ_xscale = exp(log_λ_xscale * 0.75 + 0.5),
+		λ_xshift = λ_xshift_ + 5,
+		λ_yscale = exp(log_λ_yscale * 0.75 + 0.5),
+		λ_yshift = exp(log_λ_yshift * 1.2 - 0.5),
+		μ        = exp(log_μ * 0.5),
+		δ        = exp(log_δ * 0.5)
+	) |>
+	pivot_longer(c(λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ), names_to = "Parameter", values_to = "Sample") |>
+	group_by(run, Parameter) |>
+	summarise(
+		Median = median(Sample),
+		Q_025 = quantile(Sample, 0.025),
+		Q_975 = quantile(Sample, 0.975),
+		CI_Length = Q_975 - Q_025
+	) |>
+	ungroup()
+
+ggplot() +
+	geom_histogram(aes(Median, after_stat(density), fill = "Posterior medians"), data = posterior_summaries, alpha = 0.7) +
+	geom_histogram(aes(Sample, after_stat(density), fill = "Prior"), data = prior_samples, alpha = 0.7) +
+	geom_vline(aes(xintercept = Truth), data = truth, color = "black", size = 1.5) +
+	scale_fill_manual(values = c("Posterior medians" = "dodgerblue4", "Prior" = "grey")) +
+	facet_wrap(vars(Parameter), scales = "free") +
+	labs(title = "Posterior median sampling distribution")
+
+ggsave("posterior-medians.png", width = 15, height = 8, dpi = 300)
+
+posterior_summaries |>
+	full_join(truth, by = "Parameter") |>
+	mutate(Relative_Error = (Median - Truth) / Truth) |>
+	ggplot() +
+	geom_histogram(aes(Relative_Error), fill = "dodgerblue4", alpha = 0.7) +
+	xlim(-3, 3) +
+	facet_wrap(vars(Parameter), scales = "free") +
+	labs(title = "Relative error distribution for posterior medians")
+
+ggsave("relative-errors.png", width = 15, height = 8, dpi = 300)
+
+posterior_summaries |>
+	full_join(truth, by = "Parameter") |>
+	ggplot() +
+	geom_histogram(aes(CI_Length / Truth), fill = "dodgerblue4", alpha = 0.7) +
+	expand_limits(x = 0) +
+	facet_wrap(vars(Parameter), scales = "free") +
+	labs(title = "Length distribution of 95% CIs (normalized)")
+
+ggsave("ci-lengths.png", width = 15, height = 8, dpi = 300)
+
+posterior_summaries |>
+	full_join(truth, by = "Parameter") |>
+	group_by(Parameter) |>
+	summarise(Coverage = mean((Truth >= Q_025) & (Truth <= Q_975))) |>
+	write_tsv("ci-coverage-proportions.tsv")
