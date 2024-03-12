52b69b606824697c9e4c68cd9ad6d9fa4e364652
Thanasi Bakis <abakis@uci.edu>
2024-03-12 11:57:53 -0700

    Successful beast analysis run

--- analyses/beast-analysis/main.jl
+++ analyses/beast-analysis/main.jl
@@ -1,7 +1,33 @@
 println("Loading packages...")
-using CSV, gcdyn, DataFrames, Distributions, JLD2, JSON, LinearAlgebra, StatsPlots, Turing
+using CSV, gcdyn, DataFrames, Distributions, JLD2, JSON, LinearAlgebra, Turing
 
 include("estimate_type_change_rate_matrix.jl")
+
+@model function Model(trees, Γ, ρ, state_space, present_time)
+    # Keep priors on the same scale for NUTS
+    log_λ_xscale_base ~ Normal(0, 1)
+    λ_xshift_base     ~ Normal(0, 1)
+    log_λ_yscale_base ~ Normal(0, 1)
+    log_λ_yshift_base ~ Normal(0, 1)
+    log_μ_base        ~ Normal(0, 1)
+    log_δ_base        ~ Normal(0, 1)
+
+    # Obtain our actual parameters from the proxies
+    λ_xscale = exp(log_λ_xscale_base * 0.75 + 0.5)
+    λ_xshift = λ_xshift_base * sqrt(2)
+    λ_yscale = exp(log_λ_yscale_base * 0.75 + 0.5)
+    λ_yshift = exp(log_λ_yshift_base * 1.2 - 0.5)
+    μ        = exp(log_μ_base * 0.5)
+    δ        = exp(log_δ_base * 0.5)
+
+    if DynamicPPL.leafcontext(__context__) !== Turing.PriorContext()
+		sampled_model = VaryingTypeChangeRateBranchingProcess(
+			λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ, Γ, ρ, 0, state_space, present_time
+		)
+		
+        Turing.@addlogprob! loglikelihood(sampled_model, trees)
+    end
+end
 
 function main()
 	# We will use the 10x sequences to determine the state bins and type change rate matrix.
@@ -17,63 +43,48 @@ function main()
 
 	# Compute the type change rate matrix
 	println("Computing type change rate matrix...")
-	tc_rate_matrix = compute_rate_matrix(tenx_data.sequence, state_space)
+	tc_rate_matrix = compute_rate_matrix(tenx_data.sequence, discretization_table, state_space)
 
 	# Read in the trees and do inference
 	println("Reading trees...")
-    germinal_centers = Vector{Vector{TreeNode}}()
+	germinal_centers = map(readdir("data/jld2/"; join=true)[1:5]) do file
+		trees::Vector{TreeNode} = load_object(file)
 
-	#for file in readdir("data/jld2/")
-    for file in readdir("data/jld2/")[1:2]
-        trees = load_object(joinpath("data/jld2/", file))
-        push!(germinal_centers, trees)
-    end
-
-	for gc in germinal_centers
-		for tree in gc
-			# This is my bad, I changed the event name after exporting JLD2 trees
+		for tree in trees
+			# TODO: This is my bad, I changed the event name after exporting JLD2 trees
 			for node in PostOrderTraversal(tree)
 				if node.event == :mutation
 					node.event = :type_change
 				end
 			end
 
+			# This part is fine, don't remove in the above TODO
 			map_states!(tree) do state
 				get_discretization(state, discretization_table)
 			end
 		end
+
+		trees
 	end
 
-	println("Sampling from posterior...")
-	treeset = [germinal_centers[2][1]]
-	present_time = maximum([node.t for tree in treeset for node in LeafTraversal(tree)])
+	println("Sampling from prior...")
+	prior_samples = sample(Model(nothing, tc_rate_matrix, nothing, state_space, nothing), Prior(), 100) |> DataFrame
+	CSV.write("samples-prior.csv", prior_samples)
 
-	ρ = mean(length(LeafTraversal(tree)) / 1000 for tree in treeset)
+	Threads.@threads for i in eachindex(germinal_centers)
+		name = "posterior-$i"
+		println("Sampling from $name...")
 
-	model = Model(treeset, tc_rate_matrix, ρ, state_space, present_time)
-	posterior_samples = sample(
-		model,
-		Gibbs(
-			MH(:λ => x -> LogNormal(log(x), 0.2)),
-			MH(:μ => x -> LogNormal(log(x), 0.2)),
-			MH(:δ => x -> LogNormal(log(x), 0.2))
-		),
-		10
-	) |> DataFrame
+		treeset = [germinal_centers[i][end]]
+		present_time = maximum([node.t for tree in treeset for node in LeafTraversal(tree)])
+		ρ = mean(length(LeafTraversal(tree)) / 1000 for tree in treeset)
 
-	display(posterior_samples)
+		model = Model(treeset, tc_rate_matrix, ρ, state_space, present_time)
+		posterior_samples = sample(model, NUTS(), 5000) |> DataFrame
+		CSV.write("samples-$name.csv", posterior_samples)
+	end
 
 	println("Done!")
 end
 
-@model function Model(trees, Γ, ρ, state_space, present_time)
-	λ		~ LogNormal(0, 1.2)
-    μ       ~ LogNormal(0, 1.2)
-    δ       ~ LogNormal(0, 1.2)
-
-	if DynamicPPL.leafcontext(__context__) !== Turing.PriorContext()
-		sampled_model = VaryingTypeChangeRateBranchingProcess(0, 0, 0, λ, μ, δ, Γ, ρ, 0, state_space, present_time)
-        
-		Turing.@addlogprob! loglikelihood(sampled_model, trees; reltol=1e-3, abstol=1e-3)
-    end
-end
+main()
---
+++ analyses/beast-analysis/visualize-samples.R
@@ -0,0 +1,94 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+library(glue)
+
+sigmoid <- function(x, t1, t2, t3, t4) {
+	t1 / (1 + exp(-t2 * (x - t3))) + t4
+}
+
+# Read data
+
+samples_filename <- commandArgs(trailingOnly = TRUE)[1]
+basename <- samples_filename |>
+	str_replace(".csv", "") |>
+	str_replace("samples-", "")
+
+prior_samples <- read_csv("samples-prior.csv") |>
+	mutate(
+		λ_xscale = exp(log_λ_xscale_base * 0.75 + 0.5),
+		λ_yscale = exp(log_λ_yscale_base * 0.75 + 0.5),
+		λ_xshift = λ_xshift_base * sqrt(2),
+		λ_yshift = exp(log_λ_yshift_base * 1.2 - 0.5),
+		μ        = exp(log_μ_base * 0.5),
+		δ        = exp(log_δ_base * 0.5)
+	) |>
+	select(iteration, chain, λ_xscale, λ_yscale, λ_xshift, λ_yshift, μ, δ)
+
+posterior_samples <- read_csv(samples_filename) |>
+	mutate(
+		λ_xscale = exp(log_λ_xscale_base * 0.75 + 0.5),
+		λ_yscale = exp(log_λ_yscale_base * 0.75 + 0.5),
+		λ_xshift = λ_xshift_base * sqrt(2),
+		λ_yshift = exp(log_λ_yshift_base * 1.2 - 0.5),
+		μ        = exp(log_μ_base * 0.5),
+		δ        = exp(log_δ_base * 0.5)
+	) |>
+	select(iteration, chain, λ_xscale, λ_yscale, λ_xshift, λ_yshift, μ, δ)
+
+# Plot sigmoids
+
+prior_q <- prior_samples |>
+	select(starts_with("λ_")) |>
+	pmap(\(λ_xscale, λ_yscale, λ_xshift, λ_yshift) tibble(x = seq(-5, 5, 0.05), sig = sigmoid(x, λ_xscale, λ_yscale, λ_xshift, λ_yshift))) |>
+	bind_rows(.id = "Curve") |>
+	group_by(x) |>
+	summarise(median = quantile(sig, 0.5), q05 = quantile(sig, 0.05), q20 = quantile(sig, 0.2), q80 = quantile(sig, 0.8), q95 = quantile(sig, 0.95)) |>
+	mutate(Dist = "Prior")
+
+posterior_q <- posterior_samples |>
+	select(starts_with("λ_")) |>
+	pmap(\(λ_xscale, λ_yscale, λ_xshift, λ_yshift) tibble(x = seq(-5, 5, 0.05), sig = sigmoid(x, λ_xscale, λ_yscale, λ_xshift, λ_yshift))) |>
+	bind_rows(.id = "Curve") |>
+	group_by(x) |>
+	summarise(median = quantile(sig, 0.5), q05 = quantile(sig, 0.05), q20 = quantile(sig, 0.2), q80 = quantile(sig, 0.8), q95 = quantile(sig, 0.95)) |>
+	mutate(Dist = "Posterior")
+
+y_max <- 8
+
+bind_rows(prior_q, posterior_q) |>
+	mutate(Dist = factor(Dist, levels = c("Prior", "Posterior"))) |>
+	ggplot(aes(x)) +
+	geom_ribbon(aes(ymin = q05, ymax = pmin(q95, y_max), fill="95% CI"), alpha = 0.5) +
+	geom_ribbon(aes(ymin = q20, ymax = q80, fill="80% CI"), alpha = 0.5) +
+	geom_line(aes(y = median, fill="Median"), color="dodgerblue4", linewidth=1.5) +
+	scale_fill_manual(values = c("95% CI" = "grey", "80% CI" = "#979696", "Median" = "dodgerblue4"), name = NULL) +
+	ylim(0, y_max) +
+	ylab(expression(lambda[x])) +
+	ggtitle("Birth rate sigmoid distribution") +
+	theme_bw(base_size = 32) +
+	theme(legend.position = "bottom") +
+	facet_wrap(vars(Dist))
+
+ggsave(glue("sigmoids-{basename}.png"), width = 15, height = 9, dpi=300)
+
+# Plot histograms
+
+prior_samples <- prior_samples |>
+	pivot_longer(c(λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ), names_to = "Parameter", values_to = "Sample")
+
+posterior_samples <- posterior_samples |>
+	pivot_longer(c(λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ), names_to = "Parameter", values_to = "Sample")
+	
+
+ggplot() +
+	geom_histogram(aes(Sample, after_stat(density), fill = "Posterior"), data = posterior_samples, alpha = 0.7) +
+	geom_histogram(aes(Sample, after_stat(density), fill = "Prior"), data = prior_samples, alpha = 0.7) +
+	scale_fill_manual(values = c("Posterior" = "dodgerblue4", "Prior" = "grey")) +
+	facet_wrap(vars(Parameter), scales = "free") +
+	theme_bw(base_size = 32) +
+	theme(legend.position = "bottom") +
+	labs(title = "Posterior histograms")
+
+ggsave(glue("histograms-{basename}.png"), width = 18, height = 12, dpi = 300)
+
