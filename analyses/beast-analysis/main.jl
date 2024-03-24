4fd53bab3d5d19079852afe886147574d64fe5eb
Thanasi Bakis <abakis@uci.edu>
2024-03-24 08:38:17 -0700

    GC-specific rho estimation

--- analyses/beast-analysis/main.jl
+++ analyses/beast-analysis/main.jl
@@ -3,7 +3,7 @@ using CSV, gcdyn, DataFrames, Distributions, JLD2, JSON, LinearAlgebra, Turing
 
 include("estimate_type_change_rate_matrix.jl")
 
-@model function Model(trees, Γ, ρ, state_space, present_time)
+@model function Model(trees, Γ, type_space, present_time)
     # Keep priors on the same scale for NUTS
     log_λ_xscale_base ~ Normal(0, 1)
     λ_xshift_base     ~ Normal(0, 1)
@@ -21,61 +21,68 @@ include("estimate_type_change_rate_matrix.jl")
     δ        = exp(log_δ_base * 0.5)
 
     if DynamicPPL.leafcontext(__context__) !== Turing.PriorContext()
-		sampled_model = VaryingTypeChangeRateBranchingProcess(
-			λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ, Γ, ρ, 0, state_space, present_time
-		)
-		
-        Turing.@addlogprob! loglikelihood(sampled_model, trees)
+		for tree in trees
+			ρ = length(LeafTraversal(tree)) / 1000
+			sampled_model = VaryingTypeChangeRateBranchingProcess(
+				λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ, Γ, ρ, 0, type_space, present_time
+			)
+			
+			Turing.@addlogprob! loglikelihood(sampled_model, tree)
+		end
     end
 end
 
 function main()
-	# We will use the 10x sequences to determine the state bins and type change rate matrix.
+	# We will use the 10x sequences to determine the type bins and type change rate matrix.
 	# Note that the heavy and light chain sequences each have an extra character and need correcting
-	println("Computing state bins...")
+	println("Computing type bins...")
 	tenx_data = CSV.read("../../lib/gcreplay/analysis/output/10x/data.csv", DataFrame)
 	tenx_data.sequence = chop.(tenx_data.nt_seq_H) .* chop.(tenx_data.nt_seq_L)
 	tenx_data.time = tenx_data.var"time (days)"
 
-	# Compute the state bins
+	# Compute the type bins
 	discretization_table = compute_discretization_table(tenx_data.delta_bind_CGG)
-	state_space = values(discretization_table) |> collect |> sort
+	type_space = values(discretization_table) |> collect |> sort
 
 	# Compute the type change rate matrix
 	println("Computing type change rate matrix...")
-	tc_rate_matrix = compute_rate_matrix(tenx_data.sequence, discretization_table, state_space)
+	tc_rate_matrix = compute_rate_matrix(tenx_data.sequence, discretization_table, type_space)
 
 	# Read in the trees and do inference
 	println("Reading trees...")
-	germinal_centers = map(readdir("data/jld2/"; join=true)[1:5]) do file
-		trees::Vector{TreeNode} = load_object(file)
+	treeset = map(readdir("data/jld2/"; join=true)) do germinal_center_dir
+		tree::TreeNode = load_object(joinpath(germinal_center_dir, "tree-5000.jld2"))
 
-		for tree in trees
-			map_states!(tree) do state
-				get_discretization(state, discretization_table)
-			end
+		map_types!(tree) do type
+			get_discretization(type, discretization_table)
 		end
 
-		trees
+		tree
 	end
 
 	println("Sampling from prior...")
-	prior_samples = sample(Model(nothing, tc_rate_matrix, nothing, state_space, nothing), Prior(), 100) |> DataFrame
+	prior_samples = sample(Model(nothing, tc_rate_matrix, type_space, nothing), Prior(), 5000) |> DataFrame
 	mkpath("out")
 	CSV.write("out/samples-prior.csv", prior_samples)
 
-	Threads.@threads for i in eachindex(germinal_centers)
-		name = "posterior-$i"
-		println("Sampling from $name...")
-
-		treeset = [germinal_centers[i][end]]
-		present_time = maximum([node.t for tree in treeset for node in LeafTraversal(tree)])
-		ρ = mean(length(LeafTraversal(tree)) / 1000 for tree in treeset)
-
-		model = Model(treeset, tc_rate_matrix, ρ, state_space, present_time)
-		posterior_samples = sample(model, NUTS(), 5000) |> DataFrame
-		CSV.write("out/samples-$name.csv", posterior_samples)
-	end
+	println("Sampling from posterior...")
+	present_time = maximum(node.time for tree in treeset for node in LeafTraversal(tree))
+	
+	model = Model(treeset, tc_rate_matrix, type_space, present_time)
+	# posterior_samples = sample(model, NUTS(), 2) |> DataFrame
+	posterior_samples = sample(
+       model,
+       Gibbs(
+           MH(:log_λ_xscale_base => x -> Normal(x, 0.1)),
+           MH(:λ_xshift_base     => x -> Normal(x, 0.1)),
+           MH(:log_λ_yscale_base => x -> Normal(x, 0.1)),
+           MH(:log_λ_yshift_base => x -> Normal(x, 0.1)),
+           MH(:log_μ_base        => x -> Normal(x, 0.1)),
+           MH(:log_δ_base        => x -> Normal(x, 0.1)),
+       ),
+		10000,
+	) |> DataFrame
+	CSV.write("out/samples-posterior.csv", posterior_samples)
 
 	println("Done!")
 end
--- analyses/beast-analysis/visualize-samples.R
+++ analyses/beast-analysis/visualize-samples.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
-library(tidyverse)
+cat("Loading packages...\n")
+suppressPackageStartupMessages(library(tidyverse))
 library(glue)
 
 sigmoid <- function(x, t1, t2, t3, t4) {
@@ -9,12 +10,8 @@ sigmoid <- function(x, t1, t2, t3, t4) {
 
 # Read data
 
-samples_filename <- commandArgs(trailingOnly = TRUE)[1]
-basename <- samples_filename |>
-	str_replace(".csv", "") |>
-	str_replace("out/samples-", "")
-
-prior_samples <- read_csv("out/samples-prior.csv") |>
+cat("Reading data...\n")
+prior_samples <- read_csv("out/samples-prior.csv", show_col_types = FALSE) |>
 	mutate(
 		λ_xscale = exp(log_λ_xscale_base * 0.75 + 0.5),
 		λ_yscale = exp(log_λ_yscale_base * 0.75 + 0.5),
@@ -25,7 +22,10 @@ prior_samples <- read_csv("out/samples-prior.csv") |>
 	) |>
 	select(iteration, chain, λ_xscale, λ_yscale, λ_xshift, λ_yshift, μ, δ)
 
-posterior_samples <- read_csv(samples_filename) |>
+arg <- commandArgs(trailingOnly = TRUE)
+filename <- ifelse(length(arg) > 0, arg[1], "out/samples-posterior.csv")
+posterior_samples <- read_csv(filename, show_col_types = FALSE) |>
+	filter(iteration > 1000) |>
 	mutate(
 		λ_xscale = exp(log_λ_xscale_base * 0.75 + 0.5),
 		λ_yscale = exp(log_λ_yscale_base * 0.75 + 0.5),
@@ -35,23 +35,43 @@ posterior_samples <- read_csv(samples_filename) |>
 		δ        = exp(log_δ_base * 0.5)
 	) |>
 	select(iteration, chain, λ_xscale, λ_yscale, λ_xshift, λ_yshift, μ, δ)
+
+cat("Visualizing...\n")
 
 # Plot sigmoids
 
 prior_q <- prior_samples |>
 	select(starts_with("λ_")) |>
-	pmap(\(λ_xscale, λ_yscale, λ_xshift, λ_yshift) tibble(x = seq(-5, 5, 0.05), sig = sigmoid(x, λ_xscale, λ_yscale, λ_xshift, λ_yshift))) |>
+	pmap(
+		\(λ_xscale, λ_yscale, λ_xshift, λ_yshift)
+		tibble(x = seq(-5, 5, 0.05), sig = sigmoid(x, λ_xscale, λ_yscale, λ_xshift, λ_yshift))
+	) |>
 	bind_rows(.id = "Curve") |>
 	group_by(x) |>
-	summarise(median = quantile(sig, 0.5), q05 = quantile(sig, 0.05), q20 = quantile(sig, 0.2), q80 = quantile(sig, 0.8), q95 = quantile(sig, 0.95)) |>
+	summarise(
+		median = quantile(sig, 0.5),
+		q05 = quantile(sig, 0.05),
+		q20 = quantile(sig, 0.2),
+		q80 = quantile(sig, 0.8),
+		q95 = quantile(sig, 0.95)
+	) |>
 	mutate(Dist = "Prior")
 
 posterior_q <- posterior_samples |>
 	select(starts_with("λ_")) |>
-	pmap(\(λ_xscale, λ_yscale, λ_xshift, λ_yshift) tibble(x = seq(-5, 5, 0.05), sig = sigmoid(x, λ_xscale, λ_yscale, λ_xshift, λ_yshift))) |>
+	pmap(
+		\(λ_xscale, λ_yscale, λ_xshift, λ_yshift)
+		tibble(x = seq(-5, 5, 0.05), sig = sigmoid(x, λ_xscale, λ_yscale, λ_xshift, λ_yshift))
+	) |>
 	bind_rows(.id = "Curve") |>
 	group_by(x) |>
-	summarise(median = quantile(sig, 0.5), q05 = quantile(sig, 0.05), q20 = quantile(sig, 0.2), q80 = quantile(sig, 0.8), q95 = quantile(sig, 0.95)) |>
+	summarise(
+		median = quantile(sig, 0.5),
+		q05 = quantile(sig, 0.05),
+		q20 = quantile(sig, 0.2),
+		q80 = quantile(sig, 0.8),
+		q95 = quantile(sig, 0.95)
+	) |>
 	mutate(Dist = "Posterior")
 
 y_max <- 8
@@ -59,10 +79,20 @@ y_max <- 8
 bind_rows(prior_q, posterior_q) |>
 	mutate(Dist = factor(Dist, levels = c("Prior", "Posterior"))) |>
 	ggplot(aes(x)) +
-	geom_ribbon(aes(ymin = q05, ymax = pmin(q95, y_max), fill="95% CI"), alpha = 0.5) +
-	geom_ribbon(aes(ymin = q20, ymax = q80, fill="80% CI"), alpha = 0.5) +
-	geom_line(aes(y = median, fill="Median"), color="dodgerblue4", linewidth=1.5) +
-	scale_fill_manual(values = c("95% CI" = "grey", "80% CI" = "#979696", "Median" = "dodgerblue4"), name = NULL) +
+	geom_ribbon(
+		aes(ymin = q05, ymax = pmin(q95, y_max), fill = "95% CI"),
+		alpha = 0.5
+	) +
+	geom_ribbon(aes(ymin = q20, ymax = q80, fill = "80% CI"), alpha = 0.5) +
+	geom_line(
+		aes(y = median, fill = "Median"),
+		color = "dodgerblue4",
+		linewidth = 1.5
+	) +
+	scale_fill_manual(
+		values = c("95% CI" = "grey", "80% CI" = "#979696", "Median" = "dodgerblue4"),
+		name = NULL
+	) +
 	ylim(0, y_max) +
 	ylab(expression(lambda[x])) +
 	ggtitle("Birth rate sigmoid distribution") +
@@ -70,25 +100,64 @@ bind_rows(prior_q, posterior_q) |>
 	theme(legend.position = "bottom") +
 	facet_wrap(vars(Dist))
 
-ggsave(glue("out/sigmoids-{basename}.png"), width = 15, height = 9, dpi=300)
+ggsave("out/sigmoids-posterior.png", width = 15, height = 9, dpi = 300)
 
 # Plot histograms
 
 prior_samples <- prior_samples |>
-	pivot_longer(c(λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ), names_to = "Parameter", values_to = "Sample")
+	pivot_longer(
+		c(λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ),
+		names_to = "Parameter",
+		values_to = "Sample"
+	)
 
 posterior_samples <- posterior_samples |>
-	pivot_longer(c(λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ), names_to = "Parameter", values_to = "Sample")
-	
+	pivot_longer(
+		c(λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ),
+		names_to = "Parameter",
+		values_to = "Sample"
+	)
+
 
 ggplot() +
-	geom_histogram(aes(Sample, after_stat(density), fill = "Posterior"), data = posterior_samples, alpha = 0.7) +
-	geom_histogram(aes(Sample, after_stat(density), fill = "Prior"), data = prior_samples, alpha = 0.7) +
+	geom_histogram(
+		aes(Sample, after_stat(density), fill = "Posterior"),
+		data = posterior_samples,
+		alpha = 0.7
+	) +
+	geom_histogram(
+		aes(Sample, after_stat(density), fill = "Prior"),
+		data = prior_samples,
+		alpha = 0.7
+	) +
 	scale_fill_manual(values = c("Posterior" = "dodgerblue4", "Prior" = "grey")) +
 	facet_wrap(vars(Parameter), scales = "free") +
 	theme_bw(base_size = 32) +
 	theme(legend.position = "bottom") +
 	labs(title = "Posterior histograms")
 
-ggsave(glue("out/histograms-{basename}.png"), width = 18, height = 12, dpi = 300)
+ggsave(
+	"out/histograms-posterior.png",
+	width = 18,
+	height = 12,
+	dpi = 300
+)
 
+# Plot posterior traceplots
+
+posterior_samples |>
+	ggplot(aes(iteration, Sample)) +
+	geom_line() +
+	facet_wrap(vars(Parameter), scales = "free") +
+	theme_bw(base_size = 32) +
+	theme(legend.position = "bottom") +
+	labs(title = "Posterior traceplots")
+
+ggsave(
+	"out/traceplots-posterior.png",
+	width = 22,
+	height = 12,
+	dpi = 300
+)
+
+cat("Done!\n")
