a60468ff2df46837bb4893101e90c3e864059292
Thanasi Bakis <abakis@uci.edu>
2024-03-28 09:23:29 -0700

    Consistent naming

--- analyses/simulation-study/main.jl
+++ analyses/simulation-study/main.jl
@@ -2,7 +2,7 @@ println("Loading packages...")
 
 using gcdyn, CSV, DataFrames, Random, Turing
 
-@model function Model(trees, Γ, state_space, present_time)
+@model function Model(trees, Γ, type_space, present_time)
     # Keep priors on the same scale for NUTS
     log_λ_xscale ~ Normal(0, 1)
     λ_xshift⁻    ~ Normal(0, 1)
@@ -21,7 +21,7 @@ using gcdyn, CSV, DataFrames, Random, Turing
 
     if DynamicPPL.leafcontext(__context__) !== Turing.PriorContext()
         sampled_model = VaryingTypeChangeRateBranchingProcess(
-            λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ, Γ, 1, 0, state_space, present_time
+            λ_xscale, λ_xshift, λ_yscale, λ_yshift, μ, δ, Γ, 1, 0, type_space, present_time
         )
 
         Turing.@addlogprob! loglikelihood(sampled_model, trees)
@@ -40,10 +40,10 @@ function main()
     dfs = Vector{DataFrame}(undef, num_treesets)
 
     Threads.@threads for i in 1:num_treesets
-        treeset = rand_tree(truth, num_trees_per_set, truth.state_space[1]);
+        treeset = rand_tree(truth, num_trees_per_set, truth.type_space[1]);
 
         dfs[i] = sample(
-            Model(treeset, truth.Γ, truth.state_space, truth.present_time),
+            Model(treeset, truth.Γ, truth.type_space, truth.present_time),
             NUTS(),
             5000
         ) |> DataFrame
