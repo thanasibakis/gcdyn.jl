db332cb3755db147adf26ff8bee8d230e4bc2c3c
Thanasi Bakis <abakis@uci.edu>
2024-05-03 16:30:13 -0700

    Adjust type space computation

--- analyses/beast-analysis/compute-type-space.jl
+++ analyses/beast-analysis/compute-type-space.jl
@@ -1,19 +1,53 @@
-using CategoricalArrays, CSV, DataFrames, Distributions, LinearAlgebra
+using gcdyn, CategoricalArrays, CSV, DataFrames, Distributions, JLD2, LinearAlgebra
 
 function main()
-	# We will use the 10x sequences to determine the type bins and type change rate matrix.
-	# Note that the heavy and light chain sequences each have an extra character and need correcting
+	# Note that the 10x heavy and light chain sequences each have an extra character and need correcting
 	tenx_data = CSV.read("../../lib/gcreplay/analysis/output/10x/data.csv", DataFrame)
 	tenx_data.sequence = chop.(tenx_data.nt_seq_H) .* chop.(tenx_data.nt_seq_L)
 	tenx_data.time = tenx_data.var"time (days)"
 
-	# Compute the type bins
+	# Remove sequences observed after 20 days (the length of our experiment)
+	filter!(:time => <=(20), tenx_data)
+
+	# Compute the type bins using the 10x sequences
 	discretization_table = compute_discretization_table(tenx_data.delta_bind_CGG)
 	type_space = values(discretization_table) |> collect |> sort
 
 	# Compute the type change rate matrix
 	tc_rate_matrix = compute_rate_matrix(tenx_data.sequence, discretization_table, type_space)
 
+	println("Using 10x data")
+	println("--------------")
+	println("type_space = ", repr(type_space))
+	println("discretization_table = ", repr(discretization_table))
+	println("Γ = ", repr(tc_rate_matrix))
+
+	# Also compute the type space using the BEAST sequences
+	beast_trees = map(readdir("data/jld2-with-affinities/"; join=true)) do germinal_center_dir
+		tree::TreeNode{Float64} = load_object(joinpath(germinal_center_dir, "tree-STATE_50000000.jld2"))
+	end
+	beast_affinities = [node.type for tree in beast_trees for node in PostOrderTraversal(tree)]
+
+	discretization_table = compute_discretization_table(beast_affinities)
+	type_space = values(discretization_table) |> collect |> sort
+	tc_rate_matrix = compute_rate_matrix(tenx_data.sequence, discretization_table, type_space)
+
+	println("\nUsing BEAST data")
+	println("--------------")
+	println("type_space = ", repr(type_space))
+	println("discretization_table = ", repr(discretization_table))
+	println("Γ = ", repr(tc_rate_matrix))
+
+	# Also try using the 10x sequences whose affinities fall within the 10% to 90% quantiles of the BEAST affinities
+	q10, q90 = quantile(beast_affinities, [0.1, 0.9])
+	tenx_affinities = filter(x -> q10 <= x <= q90, tenx_data.delta_bind_CGG)
+
+	discretization_table = compute_discretization_table(tenx_affinities)
+	type_space = values(discretization_table) |> collect |> sort
+	tc_rate_matrix = compute_rate_matrix(tenx_data.sequence, discretization_table, type_space)
+
+	println("\nUsing 10x and BEAST data combined")
+	println("--------------")
 	println("type_space = ", repr(type_space))
 	println("discretization_table = ", repr(discretization_table))
 	println("Γ = ", repr(tc_rate_matrix))
