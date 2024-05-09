eb2b666f16cff3788913a4cadfeb0ee50fe6f6c4
Thanasi Bakis <abakis@uci.edu>
2024-05-08 17:00:31 -0700

    Re-run demo

--- analyses/demo/main.jl
+++ analyses/demo/main.jl
@@ -1,14 +1,22 @@
 println("Loading packages...")
-
 using gcdyn, CSV, DataFrames, Optim, Random, Turing
 
 @model function Model(trees, Γ, type_space, present_time)
-    λ_xscale  ~ Gamma(2, 1)
-    λ_xshift  ~ Normal(5, 1)
-    λ_yscale  ~ Gamma(2, 1)
-    λ_yshift  ~ Gamma(1, 1)
-    μ       ~ LogNormal(0, 0.5)
-    δ       ~ LogNormal(0, 0.5)
+    # Keep priors on the same scale for NUTS
+    log_λ_xscale ~ Normal(0, 1)
+    λ_xshift⁻    ~ Normal(0, 1)
+    log_λ_yscale ~ Normal(0, 1)
+    log_λ_yshift ~ Normal(0, 1)
+    log_μ        ~ Normal(0, 1)
+    log_δ        ~ Normal(0, 1)
+
+    # Obtain our actual parameters from the proxies
+    λ_xscale = exp(log_λ_xscale * 0.75 + 0.5)
+    λ_xshift = λ_xshift⁻ + 5
+    λ_yscale = exp(log_λ_yscale * 0.75 + 0.5)
+    λ_yshift = exp(log_λ_yshift * 1.2 - 0.5)
+    μ        = exp(log_μ * 0.5)
+    δ        = exp(log_δ * 0.5)
 
     if DynamicPPL.leafcontext(__context__) !== Turing.PriorContext()
         sampled_model = SigmoidalBranchingProcess(
@@ -23,7 +31,6 @@ function main()
     Random.seed!(1)
 
     println("Setting up model...")
-
     type_space = [2, 4, 6, 8]
     Γ = [-1 0.5 0.25 0.25; 2 -4 1 1; 2 2 -5 1; 0.125 0.125 0.25 -0.5]
     present_time = 3
@@ -32,10 +39,10 @@ function main()
 
     model = Model(trees, Γ, type_space, present_time)
 
-    num_leaves = sort([length(LeafTraversal(tree)) for tree in trees])
-    num_nodes = sort([length(PostOrderTraversal(tree)) for tree in trees])
-
     open("tree-summary.txt", "w") do file
+        num_leaves = sort([length(LeafTraversal(tree)) for tree in trees])
+        num_nodes = sort([length(PostOrderTraversal(tree)) for tree in trees])
+
         println(file, "Leaf count distribution:")
         println(file, join(num_leaves, ", "), "\n")
         println(file, "Node count distribution:")
@@ -43,22 +50,10 @@ function main()
     end
 
     println("Computing initial MCMC state...")
-    max_a_posteriori = optimize(model, MAP())
+    max_a_posteriori = optimize(model, MAP(), NelderMead())
 
     println("Sampling from posterior...")
-    posterior_samples = sample(
-        model,
-        Gibbs(
-            MH(:λ_xscale => x -> LogNormal(log(x), 0.2)),
-            MH(:λ_xshift => x -> Normal(x, 0.7)),
-            MH(:λ_yscale => x -> LogNormal(log(x), 0.2)),
-            MH(:λ_yshift => x -> LogNormal(log(x), 0.2)),
-            MH(:μ => x -> LogNormal(log(x), 0.2)),
-            MH(:δ => x -> LogNormal(log(x), 0.2)),
-        ),
-        5000,
-        init_params=max_a_posteriori
-    ) |> DataFrame
+    posterior_samples = sample(model, NUTS(adtype=AutoForwardDiff(chunksize=6)), 1000, init_params=max_a_posteriori) |> DataFrame
 
     println("Exporting samples...")
     CSV.write("posterior-samples.csv", posterior_samples)
