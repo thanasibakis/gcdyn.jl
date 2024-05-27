8eefc4bea1fb55c3826c0d78b4e7ded3d2d875ab
Thanasi Bakis <abakis@uci.edu>
2024-05-26 19:34:56 -0700

    Need to output samples from bias analysis

--- analyses/bias-analysis/main.jl
+++ analyses/bias-analysis/main.jl
@@ -1,10 +1,10 @@
-using gcdyn, Turing, StatsPlots
+using gcdyn, JLD2, Turing
 
 function main()
     truth = ConstantBranchingProcess(1.8, 1, 0, 1, 0, [1])
     chns = run_simulations(truth, 2)
-    visualize_original(chns, truth)
-    visualize_both(chns, truth)
+    
+    save_object("chns.jld2", chns)
 end
 
 @model function ConditionedModel(trees, present_time)
@@ -73,92 +73,6 @@ function run_simulations(truth, present_time)
     end
 
     chns
-end
-
-function visualize_original(chns, truth)
-    λ_plts = []
-    μ_plts = []
-
-    for n in sort(collect(keys(chns)))
-        chns_dict = chns[n]
-
-        # Birth rate plots
-        medians_original = map(chns_dict[:original]) do chn
-            median(get(chn, :λ)[1])
-        end
-
-        plt = histogram(medians_original; alpha=0.7, label="Original", normalize=:pdf, fill="grey")
-        vline!([truth.λ]; label="Truth", linewidth=4, color="orange")
-        xlims!(0, 3)
-        title!("$n trees", titlefontsize=10)
-
-        push!(λ_plts, plt)
-
-        # Death rate plots
-        medians_original = map(chns_dict[:original]) do chn
-            median(get(chn, :μ)[1])
-        end
-
-        plt = histogram(medians_original; alpha=0.7, label="Original", normalize=:pdf, fill="grey")
-        vline!([truth.μ]; label="Truth", linewidth=4, color="orange")
-        xlims!(0, 3)
-        title!("$n trees", titlefontsize=10)
-
-        push!(μ_plts, plt)
-    end
-
-    plot(λ_plts...; layout=(1, 4), thickness_scaling=0.75, dpi=300, size=(1000, 300), plot_title="Birth rate sampling distributions", plot_titlefontsize=12)
-    savefig("birth-rate-bias-original-only.svg")
-
-    plot(μ_plts...; layout=(1, 4), thickness_scaling=0.75, dpi=300, size=(1000, 300), plot_title="Death rate sampling distributions", plot_titlefontsize=12)
-    savefig("death-rate-bias-original-only.svg")
-end
-
-function visualize_both(chns, truth)
-    λ_plts = []
-    μ_plts = []
-
-    for n in sort(collect(keys(chns)))
-        chns_dict = chns[n]
-
-        # Birth rate plots
-        medians_corrected = map(chns_dict[:corrected]) do chn
-            median(get(chn, :λ)[1])
-        end
-        medians_original = map(chns_dict[:original]) do chn
-            median(get(chn, :λ)[1])
-        end
-
-        plt = histogram(medians_original; alpha=0.7, label="Original", normalize=:pdf, fill="grey")
-        histogram!(medians_corrected; alpha=0.7, label="Conditioned", normalize=:pdf, fill="lightblue")
-        vline!([truth.λ]; label="Truth", linewidth=4, color="orange")
-        xlims!(0, 3)
-        title!("$n trees", titlefontsize=10)
-
-        push!(λ_plts, plt)
-
-        # Death rate plots
-        medians_corrected = map(chns_dict[:corrected]) do chn
-            median(get(chn, :μ)[1])
-        end
-        medians_original = map(chns_dict[:original]) do chn
-            median(get(chn, :μ)[1])
-        end
-
-        plt = histogram(medians_original; alpha=0.7, label="Original", normalize=:pdf, fill="grey")
-        histogram!(medians_corrected; alpha=0.7, label="Conditioned", normalize=:pdf, fill="lightblue")
-        vline!([truth.μ]; label="Truth", linewidth=4, color="orange")
-        xlims!(0, 3)
-        title!("$n trees", titlefontsize=10)
-
-        push!(μ_plts, plt)
-    end
-
-    plot(λ_plts...; layout=(1, 4), thickness_scaling=0.75, dpi=300, size=(1000, 300), plot_title="Birth rate sampling distributions", plot_titlefontsize=12)
-    savefig("birth-rate-bias.svg")
-
-    plot(μ_plts...; layout=(1, 4), thickness_scaling=0.75, dpi=300, size=(1000, 300), plot_title="Death rate sampling distributions", plot_titlefontsize=12)
-    savefig("death-rate-bias.svg")
 end
 
 main()
